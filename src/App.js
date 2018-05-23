c6cbed4e7d4fb4666eb854ec2e48cb7b2a10e316
Ivan Zmerzlyi <dangelzm@gmail.com>
2018-05-23 15:28:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +6,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React ({process.env.NODE_ENV})</h2>
           <p>APIHOST = {process.env.REACT_APP_APIHOST}</p>
         </div>
